a2c16b8e1defd32ed3dfdc613f3d1106a5044183
Cheikh Diagne <43870170+cheikhkeydi@users.noreply.github.com>
2023-03-03 17:57:08 +0100

    Panel V1

---
+++ src/app/Module/Shared/Menu/side-bar/side-bar.component.ts
@@ -0,0 +1,40 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-side-bar',
+  templateUrl: './side-bar.component.html',
+  styleUrls: ['./side-bar.component.css']
+})
+export class SideBarComponent {
+
+  // ======== BTN SIDEBAR css=> open/close ==========
+
+  iconToggle = "";
+  public toggle: boolean = true;
+
+  public togggle() {
+    this.toggle = !this.toggle;
+  }
+  public toggleNav(): string {
+    if (this.toggle) {
+      this.iconToggle = "fiber_manual_record"
+      return ""
+    }
+    else {
+      this.iconToggle = ""
+      return "close"
+    }
+  }
+
+// ============================================
+
+  ngAfterViewInit() {
+    document.querySelectorAll('.index-arrow').forEach((arrowEl: { addEventListener: (arg0: string, arg1: (e: any) => void) => void; }, i: any) => {
+      arrowEl.addEventListener('click', (e) => {
+        const arrowParent = e.target.parentElement.parentElement;
+        arrowParent.classList.toggle('showMenu');
+      });
+    });
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PanelComponent } from './module/panel/panel.component';
+import { NavBarComponent } from './module/Shared/Menu/nav-bar/nav-bar.component';
+import { SideBarComponent } from './module/Shared/Menu/side-bar/side-bar.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatModule } from './mat.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PanelComponent,
+    NavBarComponent,
+    SideBarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatModule
   ],
   providers: [],
   bootstrap: [AppComponent]
